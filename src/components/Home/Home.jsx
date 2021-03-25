7384bbe71d9507ca5ecc3d6be73b638bc0745ff0
jeyofdev <jgregoire.pro@gmail.com>
2021-03-25 10:19:53 +0100

    feat : add the Layout component

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import Header from '../Ui/Header/Header';
 import ImageSrc from '../../assets/images/header.svg';
+import Layout from '../Ui/Layout/Layout';
 
 const Home = () => {
-    return <Header title="Country quiz" imageSrc={ImageSrc} />;
+    return (
+        <>
+            <Header title="Country quiz" imageSrc={ImageSrc} />
+            <Layout>layout</Layout>
+        </>
+    );
 };
 
 export default Home;
