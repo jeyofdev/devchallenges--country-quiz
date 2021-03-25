285860e673b83ae82415790db4766d619312c663
jeyofdev <jgregoire.pro@gmail.com>
2021-03-25 09:45:13 +0100

    feat : initialize the router

--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from '../../components/Home/Home';
+import Quiz from '../../components/Quiz/Quiz';
 import './App.css';
 
 const App = () => {
     return (
         <div className="App">
-            <div className="container">App</div>
+            <div className="container">
+                <Router>
+                    <Switch>
+                        <Route exact path="/" component={Home} />
+                        <Route exact path="/quiz" component={Quiz} />
+                    </Switch>
+                </Router>
+            </div>
         </div>
     );
 };
